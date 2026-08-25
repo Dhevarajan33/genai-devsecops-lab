7dff91c1ce525f1dd70cbe2a3f8ecb7740e7ddf5
Dhevarajan33 <dhevarajanp.adriottech@gmail.com>
2026-08-25 14:43:12 +0530

    Add tests for model utilities functions

--- test_model_utils.py
+++ test_model_utils.py
@@ -1,13 +1,16 @@
 from model_utils import load_fake_dataset, train_fake_model, validate_model
 
+
 def test_load_fake_dataset():
     data = load_fake_dataset()
     assert data["rows"] == 1000
+
 
 def test_train_fake_model():
     data = load_fake_dataset()
     model = train_fake_model(data)
     assert 0 <= model["accuracy"] <= 1
+
 
 def test_validate_model_passes_threshold():
     model = {"accuracy": 0.87}
